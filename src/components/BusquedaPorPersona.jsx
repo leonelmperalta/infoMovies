155fc61162e8d3b5969b9fcd4c29660d01498805
leonelmperalta <leonelm.peralta@outlook.es>
2021-01-19 18:48:47 -0300

    creada busqueda por persona

--- src/components/BusquedaPorPersona.jsx
+++ src/components/BusquedaPorPersona.jsx
@@ -1,9 +1,129 @@
-import React from 'react';
+import React, { Fragment, useState } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { TextField, Paper, Button } from "@material-ui/core";
+import ListaDePersonas from "./ListaDePersonas";
 
-const BusquedaPorPersona = () => {
-    return ( 
-        <div>BUSQUEDA PorPersona</div>
-     );
-}
- 
-export default BusquedaPorPersona;
+const styles = {
+  paper: {
+    position: "absolute",
+    top: "10em",
+    left: "5vw",
+    width: "19em",
+    height: "16em",
+    padding: "10px",
+    display: "inline-block",
+  },
+  textfield: {
+    margin: "10px",
+  },
+  button: {
+    left: "10.5em",
+    top: ".8em",
+    backgroundColor: "black",
+    "&:hover": {
+      background: "gray",
+    },
+  },
+};
+
+const BusquedaPorPersona = (props) => {
+  const { classes } = props;
+
+  //State para guardar la busqueda actual
+  const [busqueda, setBusqueda] = useState({
+    nombre: "",
+  });
+
+  //State para guardar el resultado de la busqueda
+
+  const [resultadoBusqueda, setResultadoBusqueda] = useState({});
+
+  //extraigo el valor nombre
+
+  const { nombre } = busqueda;
+
+  //funcion para validar en tiempo real los form
+
+  const handleChange = (e) => {
+    setBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //Funcion para extraer el valor de la busqueda
+
+  function realizarBusqueda() {
+    //valido los datos
+
+    if (nombre.trim() === null || nombre.trim() === "") {
+      alert("Escribi bien el nombre sorete");
+      return;
+    }
+
+    //llamo a la funcion para buscar el contenido en la api
+    consultarAPI(nombre);
+
+    // reinicio la busqueda
+    setBusqueda({ nombre: "" });
+  }
+
+  //funcion para consultar la persona en la api
+
+  async function consultarAPI(nombre) {
+    let url =
+      "https://api.themoviedb.org/3/search/person?api_key=2bfde98323b35592c98968f6ac494fc7&language=en-US&query=" +
+       nombre  +
+      "&page=1&include_adult=false";
+
+    try {
+      let resultado = {};
+      await fetch(url)
+        .then((response) => response.json())
+        .then((data) => (resultado = data));
+        console.log(resultado);
+      validarRenderPersona(resultado);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  //funcion para el caso en que la busqueda no devuelva ningun resultado.
+
+  function validarRenderPersona(resultado) {
+    if (resultado.total_results === 0) {
+      alert("no encontre nadia");
+    } else {
+      setResultadoBusqueda(resultado);
+    }
+  }
+
+  return (
+    <Fragment>
+      <Paper elevation={3} className={classes.paper}>
+        <form noValidate autoComplete="off">
+          <TextField
+            className={classes.textfield}
+            id="standard-basic"
+            name="nombre"
+            label="Nombre de la persona: "
+            variant="outlined"
+            value={nombre}
+            onChange={handleChange}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            onClick={realizarBusqueda}
+          >
+            Buscar
+          </Button>
+        </form>
+      </Paper>
+      <ListaDePersonas resultadoBusqueda={resultadoBusqueda} />
+    </Fragment>
+  );
+};
+
+export default withStyles(styles)(BusquedaPorPersona);
---
+++ src/components/ListaDePersonas.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import {
+  Link as RouterLink
+} from "react-router-dom";
+
+const styles = {
+  container: {
+    position: "absolute",
+    top: "10em",
+    width: "60em",
+    left: "32em",
+    maxHeight: "30em",
+  },
+  tableCell: {
+    textDecoration: "underline",
+    cursor: "pointer",
+  }
+};
+
+const ListaDePersonas = (props) => {
+  const { classes } = props;
+
+  // extraigo de las props el resultado de la busqueda
+  const {results} = props.resultadoBusqueda;
+
+  return (
+    <TableContainer component={Paper} className={classes.container}>
+      <Table stickyHeader size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="left">Nombre:</TableCell>
+            <TableCell align="left">Known for: </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody className= {classes.tableBody}>
+        {results === undefined ? null : results.map((persona) => (
+            <TableRow key={persona.name} >
+              <TableCell className= {classes.tableCell} component="th" scope="row" >
+                { persona.name }
+              </TableCell>
+              <TableCell align="left">{persona.known_for_department}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default withStyles(styles)(ListaDePersonas);
